e382d77dc6fffdbe6bde17c4bf39d0804b3da881
Pietro Giordano <giopi87@gmail.com>
2023-01-24 18:02:34 +0100

    aggiunta bonus 1

---
+++ bonus/js/scripts.js
@@ -0,0 +1,54 @@
+const { createApp } = Vue;
+
+createApp({
+
+      data() {
+
+            return {    
+                  toDoList: [
+                        {
+                              text: 'Corsa mattutina',
+                              done: true
+                        }, {
+                              text: 'Spesa al supermercato',
+                              done: true
+                        }, {
+                              text: 'Lavare pavimento',
+                              done: false
+                        }, {
+                              text: 'Allenarsi in palestra',
+                              done: false
+                        },
+                  ],
+                  newTask: {
+                        text: '',
+                        done: false
+                  }
+            };
+
+      },
+
+      methods: {
+            
+            eraseTask(index) {
+
+                  this.toDoList.splice(index, 1);
+                  console.log(this.toDoList);
+
+            },
+
+            createTask() {
+
+                  if(this.newTask.text != '') {
+                        
+                        this.toDoList.push({...this.newTask});
+                        console.log(this.toDoList);
+                        this.newTask.text = '';
+
+                  }
+                  
+            }
+
+      },
+
+}).mount('#app');
